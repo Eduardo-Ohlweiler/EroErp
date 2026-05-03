a96697a6271bcb26ccab819b750ccd8d74b23450
Eduardo-Ohlweiler <ohlweilereduardo@gmail.com>
2026-05-03 12:15:24 -0300

    backend/feature: Adicionado anotção de component em CidadeMapper

--- ero-erp-api/src/main/java/com/api/ero_erp/cidade/mapper/CidadeMapper.java
+++ ero-erp-api/src/main/java/com/api/ero_erp/cidade/mapper/CidadeMapper.java
@@ -2,7 +2,9 @@ package com.api.ero_erp.cidade.mapper;
 
 import com.api.ero_erp.cidade.dtos.CidadeResponseDto;
 import com.api.ero_erp.cidade.entity.Cidade;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CidadeMapper {
 
     public CidadeResponseDto toDTO(Cidade c) {
